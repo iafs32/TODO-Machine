9dfd21ffd8ffa98ade15e2625c6dab3941d6cc05
Irving Franco <iafs.0232@gmail.com>
2022-07-28 12:39:24 -0500

    Manejo del estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,15 +2,20 @@ import React from 'react'
 import './TodoSearch.css'
 
 export default function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+  
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
    
-  return (
+  return [
     <input
       className='TodoSearch'
       placeholder='Cebolla'
+      value={searchValue}
       onChange={onSearchValueChange}
-    />
-  )
+    />,
+    <p>{searchValue}</p>
+  ]
 }
