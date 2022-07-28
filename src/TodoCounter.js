97243a80dd974f9a0eeadb799be8c344a2070e8d
Irving Franco <iafs.0232@gmail.com>
2022-07-28 12:09:23 -0500

    CSS en React

---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './TodoCounter.css'
+
+
+export default function TodoCounter() {
+  return (
+    <h2 className='TodoCounter'>Has completado 2 de 3 TODOs</h2>
+  )
+}
---
+++ src/TodoSearch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './TodoSearch.css'
+
+export default function TodoSearch() {
+  return (
+    <input className='TodoSearch' placeholder='Cebolla'/>
+  )
+}
