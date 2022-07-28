8dae2e0979dfdd931d6c077caafa0a28556db4f1
Irving Franco <iafs.0232@gmail.com>
2022-07-28 12:27:14 -0500

    Manejo de eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,16 @@ import React from 'react'
 import './CreateTodoButton.css'
 
 export default function CreateTodoButton(props) {
+  const onClickButton = (msg) => {
+    alert(msg);
+  };
+  
   return (
-    <button className='CreateTodoButton'>+</button>
+    <button 
+      className='CreateTodoButton'
+      onClick={() => onClickButton('Aquí se debería abrir el modal')}
+    >
+      +
+    </button>
   )
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,15 @@ import React from 'react'
 import './TodoSearch.css'
 
 export default function TodoSearch() {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+  };
+   
   return (
-    <input className='TodoSearch' placeholder='Cebolla'/>
+    <input
+      className='TodoSearch'
+      placeholder='Cebolla'
+      onChange={onSearchValueChange}
+    />
   )
 }
